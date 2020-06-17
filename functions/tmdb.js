c4b2b7b2807754bf79f8bf98e141044c16366ac2
Darius Kruythoff <dkruythoff@gmail.com>
2020-06-17 09:10:53 +0200

    Set up tmdb api proxy

---
+++ functions/tmdb.js
@@ -0,0 +1,55 @@
+const axios = require('axios').default;
+
+const statusCode = 200;
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type'
+};
+const baseURL = 'https://api.themoviedb.org/3/'
+const api_key = process.env.TMDB_API_KEY
+
+exports.handler = async function (event) {
+
+  // CORS
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode,
+      headers
+    }
+  }
+
+  // API key check
+  if (!api_key) {
+    return {
+      statusCode: 500,
+      headers,
+      body: 'No key found'
+    }
+  }
+
+  // Path check
+  if (!event.path) {
+    return {
+      statusCode: 500,
+      headers,
+      body: 'No path given'
+    }
+  }
+
+  const body = await axios({
+    baseURL,
+    url: event.path,
+    params: {
+      ...(event.queryStringParameters || {}),
+      api_key
+    }
+  })
+    .then(({ data }) => data)
+    .catch((err) => console.log(err.message))
+
+  return {
+    statusCode,
+    headers: { ...headers, 'Content-type': 'application/json' },
+    body
+  }
+}
